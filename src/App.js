d92ae89908f93ab299adf6166d23c81e4af3f497
David <crusedmoss@gmail.com>
2022-06-15 01:22:17 -0500

    coverage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React, { useState } from 'react';
-import AuthForm from './components/AuthForm';
 
 function App() {
   const [form, setState] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -12,7 +12,7 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(form);
+    setIsSubmitted(true);
   };
   return (
     <React.Fragment>
@@ -60,7 +60,72 @@ function App() {
             </div>
 
             <div className='flex mt-8 lg:w-1/2 lg:justify-end lg:mt-0'>
-              <AuthForm handleChange={handleChange} handleSubmit={handleSubmit} />
+              <div className='w-full lg:w-3/4 bg-white rounded-lg'>
+                <div className='py-12 px-5 text-center'>
+                  <h2 className='text-2xl font-semibold text-gray-700 fo'>Sign In</h2>
+
+                  <form onSubmit={handleSubmit}>
+                    <div className='mt-4'>
+                      <input
+                        className='block w-full px-4 py-2 text-gray-700 placeholder-gray-400 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:ring-blue-300 focus:outline-none focus:ring'
+                        type='email'
+                        name='email'
+                        onChange={handleChange}
+                        placeholder='Email address'
+                        aria-label='Email address'
+                      />
+                      <input
+                        className='block w-full px-4 py-2 mt-4 text-gray-700 placeholder-gray-400 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:ring-blue-300 focus:outline-none focus:ring'
+                        type='password'
+                        name='password'
+                        onChange={handleChange}
+                        placeholder='Password'
+                        aria-label='Password'
+                      />
+                    </div>
+
+                    <div className='flex items-center justify-between mt-4'>
+                      <a href='/' className='text-sm text-gray-600 hover:underline'>
+                        Forget Password?
+                      </a>
+
+                      <button
+                        type='submit'
+                        className='px-4 py-2 font-semibold text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800'
+                      >
+                        Login
+                      </button>
+
+                      {/* Alert */}
+                      {isSubmitted && (
+                        <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800'>
+                          <div className='flex items-center justify-center w-12 bg-emerald-500'>
+                            <svg
+                              className='w-6 h-6 text-white fill-current'
+                              viewBox='0 0 40 40'
+                              xmlns='http://www.w3.org/2000/svg'
+                            >
+                              <path d='M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z' />
+                            </svg>
+                          </div>
+
+                          <div className='px-4 py-2 -mx-3'>
+                            <div className='mx-3'>
+                              <span className='font-semibold text-emerald-500 dark:text-emerald-400'>
+                                Success
+                              </span>
+                              <p className='text-sm text-gray-600 dark:text-gray-200'>
+                                Sesión inciada correctamente
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                      {/* End Alert */}
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,32 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-jest.mock('./components/AuthForm', () => ({
-  __esModule: true,
-  default: () => <div>AuthForm</div>,
-}));
+describe('<App/>', () => {
+  test('renders learn react link', () => {
+    const container = render(<App />);
+    expect(container.baseElement).toMatchSnapshot();
+  });
 
-test('renders learn react link', () => {
-  const container = render(<App />);
-  expect(container.baseElement).toMatchSnapshot();
+  it('should handle user input', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    await user.type(screen.getByPlaceholderText('Email address'), 'test@test.com');
+    await user.type(screen.getByPlaceholderText('Password'), 'password');
+
+    expect(screen.getByPlaceholderText('Email address')).toHaveValue('test@test.com');
+    expect(screen.getByPlaceholderText('Password')).toHaveValue('password');
+  });
+
+  it('should submit form', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    await user.click(screen.getByRole('button', { name: 'Login' }));
+
+    expect(screen.getByText(/Sesión inciada correctamente/)).toBeInTheDocument();
+  });
 });
