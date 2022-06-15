5b145542d93b1b8745cc069c71b1c766c30ea9ef
David <crusedmoss@gmail.com>
2022-06-15 01:03:11 -0500

    mocking components

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+
+jest.mock('./components/AuthForm', () => ({
+  __esModule: true,
+  default: () => <div>AuthForm</div>,
+}));
 
 test('renders learn react link', () => {
   const container = render(<App />);
